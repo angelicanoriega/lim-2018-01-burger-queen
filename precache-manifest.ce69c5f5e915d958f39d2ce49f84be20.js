ac49def0dcb0c6733627e1d1d453bf179d6da992
Maria <mariaanguevara@gmail.com>
2018-10-29 05:15:29 -0500

    Updates

---
+++ precache-manifest.ce69c5f5e915d958f39d2ce49f84be20.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "9db9c87299469fdf71e9",
+    "url": "/static/js/main.9db9c872.chunk.js"
+  },
+  {
+    "revision": "4fd91c74e51506835aa9",
+    "url": "/static/js/1.4fd91c74.chunk.js"
+  },
+  {
+    "revision": "9db9c87299469fdf71e9",
+    "url": "/static/css/main.47bbd46d.chunk.css"
+  },
+  {
+    "revision": "522904ab9a538eac123c6fa4f202855a",
+    "url": "/index.html"
+  }
+];
